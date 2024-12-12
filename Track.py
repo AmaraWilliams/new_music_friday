b6dde9c48d294bb7f197ab5b4a7bc15e1f989dd9
AmaraWilliams <amaraajw@gmail.com>
2024-12-12 14:21:52 -0500

    remove key and tempo parameters

--- Track.py
+++ Track.py
@@ -1,7 +1,5 @@
 class Track:
-    def __init__(self, id, uri, add_date, key, tempo):
+    def __init__(self, id, uri, add_date):
         self.id = id
         self.uri = uri
         self.add_date = add_date
-        self.key = key
-        self.tempo = tempo
