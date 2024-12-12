10b26f103e0e180250a2456febb22262caefafd9
AmaraWilliams <amaraajw@gmail.com>
2024-12-12 14:47:09 -0500

    add artists as an attribute for a track

--- Track.py
+++ Track.py
@@ -1,5 +1,6 @@
 class Track:
-    def __init__(self, id, uri, add_date):
+    def __init__(self, id, uri, add_date, artists):
         self.id = id
         self.uri = uri
         self.add_date = add_date
+        self.artists = artists
